e3cf96e4b2807b5b48c59dcb63608c0a7964a046
PrestonMasseyBlake <prestonmb2017@gmail.com>
2021-04-28 11:46:25 -0700

    fewf

--- create/urls.py
+++ create/urls.py
@@ -1,9 +1,10 @@
 from create import views
 from django.urls import path
-from .views import LoungeView, LoungeLookupView, LoungePageView, LoungePageUpdateView
+from .views import LoungeView, LoungeLookupView, LoungePageView, LoungePageUpdateView, SearchPage
 urlpatterns = [
     path('lounges',LoungeView.as_view({'get': 'list', 'post': 'create'})),
     path('edit/<slug>',LoungePageView.as_view()),
     path('update/<slug>',LoungePageUpdateView.as_view()),
-    path('<slug>',LoungeLookupView.as_view({'get':'list'}))
+    path('<slug>',LoungeLookupView.as_view({'get':'list'})),
+    path('search/<str:q>', SearchPage.as_view())
 ]
--- create/views.py
+++ create/views.py
@@ -5,7 +5,7 @@ from .serializers import LoungeSerializer, LoungePageSerializer
 from .models import Lounge
 from django.core import serializers
 from rest_framework import viewsets
-
+from django.db.models import Q
 class LoungeView(viewsets.ModelViewSet):
     queryset = Lounge.objects.all()
     serializer_class = LoungeSerializer
@@ -21,3 +21,13 @@ class LoungeLookupView(viewsets.ModelViewSet):
     queryset = Lounge.objects.all()
     serializer_class = LoungeSerializer
     lookup_field = 'slug'
+class SearchPage(ListAPIView):
+    serializer_class = LoungeSerializer
+    model = serializer_class.Meta.model
+    paginate_by = 5
+    def get_queryset(self):
+        query = self.kwargs.get('q')
+        if query:
+            return self.model.objects.filter(
+                Q(name__icontains=query)| Q(slug__icontains=query)).distinct()
+        return None
